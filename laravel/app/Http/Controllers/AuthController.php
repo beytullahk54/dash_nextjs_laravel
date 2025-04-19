9b5b4d47798b0de06d9b91c3763159d8d99ee60e
beytullah <kahramanbey54@gmail.com>
2025-04-20 01:28:58 +0300

    Laravel Sanctum ve sail kurulumu

---
+++ laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Login successful',
+            ]);
+        }
+
+        if (User::where('email', $request->email)->exists()) {
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'message' => 'Login successful',
+                'token' => $token,
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Login failed',
+        ]);
+    }
+}
