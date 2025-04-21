99b052bfb088fe2c8bb0568a2cad35bb6c5435fc
Beytullah Kahraman <32388523+beytullahk54@users.noreply.github.com>
2025-04-21 11:04:38 +0300

    Update page.tsx

--- nextjs/shacdn/src/app/(dashboard)/settings/page.tsx
+++ nextjs/shacdn/src/app/(dashboard)/settings/page.tsx
@@ -1,25 +1,9 @@
 "use client"
 
-import { useEffect, useCallback } from "react";
-import { useRouter } from 'next/router';
 
 export default function Settings() {
   const { data: session } = useSession();
-  const router = useRouter();
 
-  const fetchUsersCallback = useCallback(() => {
-    if (session?.accessToken) {
-      
-    }else{
-      router.push('/'); // Anasayfaya yönlendir
-    }
-    
-  }, [session?.accessToken]);
-
-  useEffect(() => {
-    fetchUsersCallback();
-  }, [fetchUsersCallback]);
-  
   return (
     <div className="flex-1 p-4">
         <h1 className="text-3xl font-bold mb-8">Ayarlar</h1>
